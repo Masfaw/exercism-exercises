1bbe84e579015d146a65372d47523fbb776e5f5f
Matheos Asfaw <matheos.asfaw@gmail.com>
2017-10-24 18:05:12 -0700

    finished first exercise

--- java/hello-world/src/main/java/Greeter.java
+++ java/hello-world/src/main/java/Greeter.java
@@ -1,7 +1,10 @@
 class Greeter {
 
     String getGreeting() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        return "Hello, World!";
     }
+
 
 }
