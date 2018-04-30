841a0e7cfded311b4bac38eb78c061face85ccd7
Matheos Asfaw <matheos.asfaw@gmail.com>
2018-04-30 00:19:23 -0700

    finished armstrong Numbers

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -2,7 +2,20 @@ class ArmstrongNumbers {
 
 	boolean isArmstrongNumber(int numberToCheck) {
 
+		int numDigits = String.valueOf(numberToCheck).length();
+		String numString = "" + numberToCheck;
+		int sum =0;
 
+		for (int i =0; i < numString.length(); i++){
+		    int n = Character.getNumericValue (numString.charAt(i));
+
+		    sum += (int) Math.pow(n,numDigits);
+        }
+
+        if (sum == numberToCheck){
+		    return true;
+        }
+        return false;
 		
 	}
 
